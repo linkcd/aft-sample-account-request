812df90d2915fc122456ce9ffd618dfae4791416
linkcd <linkcd@gmail.com>
2024-12-06 20:15:34 +0100

    try new account dave and also charlie

---
+++ terraform/account-request-charlie2.tf
@@ -0,0 +1,35 @@
+module "account_request_charlie" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "lufng+ct-developer-charlie@amazon.com"
+    AccountName               = "Developer Charlie"
+    ManagedOrganizationalUnit = "Sandbox" 
+    SSOUserEmail              = "lufng@fabeldyr.onmicrosoft.com"
+    SSOUserFirstName          = "Developer"
+    SSOUserLastName           = "Charlie"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "lufng+ct-developer-charlie@amazon.com"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "Dev"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "AWS Control Tower Lab"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform (AFT)"
+  }
+
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
+
+  account_customizations_name = "SANDBOX"
+}
